a4e0e8c30c8fc3de1f63c99c35f38dbdf62dd5c8
Michael Gerdes <mgerdes11@gmail.com>
2021-01-17 01:37:17 -0500

    Scripting

---
+++ libraries/mscript/env.h
@@ -0,0 +1,14 @@
+#ifndef _MSCRIPT_ENV_H
+#define _MSCRIPT_ENV_H
+
+struct mscript_function_declaration {
+};
+
+struct mscript_variable_declaration {
+};
+
+struct mscript_env {
+
+};
+
+#endif
--- libraries/mscript/parser.h
+++ libraries/mscript/parser.h
@@ -1,6 +1,28 @@
 #ifndef _MSCRIPT_PARSER_H
 #define _MSCRIPT_PARSER_H
 
+#include <stdbool.h>
+
+#define MSCRIPT_MAX_SYMBOL_LEN 16
+#define MSCRIPT_MAX_FUNCTION_ARGS 16
+#define MSCRIPT_MAX_STRUCT_MEMBERS 16
+
+enum mscript_type_type {
+    MSCRIPT_TYPE_VOID,
+    MSCRIPT_TYPE_INT,
+    MSCRIPT_TYPE_FLOAT,
+    MSCRIPT_TYPE_STRUCT,
+    MSCRIPT_TYPE_ARRAY,
+};
+
+struct mscript_type {
+    enum mscript_type_type type;
+
+    enum mscript_type_type array_type;
+    char *struct_name; 
+};
+
+struct mscript_program;
 void mscript_compile_2(const char *prog_text);
 
 #endif
