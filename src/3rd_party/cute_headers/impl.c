6a829e4793d00d47a81e595cee069600b5ab9456
Michael Gerdes <mgerdes11@gmail.com>
2021-12-03 23:55:37 -0500

    Experimenting with different file libraries

--- src/3rd_party/cute_headers/impl.c
+++ src/3rd_party/cute_headers/impl.c
@@ -1,3 +1,7 @@
 #define CUTE_FILES_IMPLEMENTATION
 #include "cute_headers/cute_files.h"
 
+#define CUTE_FILEWATCH_IMPLEMENTATION
+#include "mattiasgustavsson_libs/assetsys.h"
+#include "cute_headers/cute_filewatch.h"
+
--- src/3rd_party/mattiasgustavsson_libs/impl.c
+++ src/3rd_party/mattiasgustavsson_libs/impl.c
@@ -5,3 +5,9 @@
 
 #define RND_IMPLEMENTATION
 #include "mattiasgustavsson_libs/rnd.h"
+
+#define STRPOOL_IMPLEMENTATION
+#include "mattiasgustavsson_libs/strpool.h"
+
+#define ASSETSYS_IMPLEMENTATION
+#include "mattiasgustavsson_libs/assetsys.h"
